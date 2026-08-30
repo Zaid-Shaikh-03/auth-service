fdab29ade030e0530a58a8c56af99035b396c779
Zaid <zaidshaikh0024@gmail.com>
2026-08-31 02:52:48 +0530

    feat: update package.json for TypeORM integration and add database configuration
    - Added TypeORM and reflect-metadata to dependencies.
    - Updated scripts for development and added typeorm command.
    - Enhanced dotenv configuration to load environment-specific .env files.
    - Created data source configuration for TypeORM with PostgreSQL.
    - Added User entity with a primary key.

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,20 @@
 import { config } from "dotenv";
 import { z } from "zod";
+import path from "path";
 
-config();
+config({
+    path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`),
+});
 
 const envSchema = z.object({
     PORT: z.coerce.number().int().positive(),
     NODE_ENV: z.enum(["development", "production", "test"]),
+
+    DB_HOST: z.string().min(1),
+    DB_PORT: z.coerce.number().int().positive(),
+    DB_USERNAME: z.string().min(1),
+    DB_PASSWORD: z.string().min(1),
+    DB_NAME: z.string().min(1),
 });
 
 const parsedEnv = envSchema.safeParse(process.env);
@@ -26,4 +35,9 @@ if (!parsedEnv.success) {
 export const Config = Object.freeze({
     PORT: parsedEnv.data.PORT,
     NODE_ENV: parsedEnv.data.NODE_ENV,
+    DB_HOST: parsedEnv.data.DB_HOST,
+    DB_PORT: parsedEnv.data.DB_PORT,
+    DB_USERNAME: parsedEnv.data.DB_USERNAME,
+    DB_PASSWORD: parsedEnv.data.DB_PASSWORD,
+    DB_NAME: parsedEnv.data.DB_NAME,
 });
